1e1edecad051ee2bafd41e4f9524b8cd54966d3d
Mads Kristensen <post@madskristensen.net>
2015-04-12 15:49:12 -0700

    Updates

--- src/timescore.js
+++ src/timescore.js
@@ -3,8 +3,6 @@
     var _hour, _minute, _date;
 
     function getScore(date) {
-        /// <param name="date" value="new Date()" />
-
         normalize(date);
         var score = runRules();
 
@@ -17,7 +15,7 @@
     function normalize(date) {
         var minute = date.getMinutes();
         var hour = date.getHours();
-        
+
         if (minute < 10) {
             _minute = "0" + minute;
         }
@@ -30,17 +28,16 @@
         }
         else if (hour > 12) {
             var imperial = hour - 12;
-            
+
             if (imperial < 10) {
                 imperial = "0" + imperial;
             }
-            
+
             _hour = imperial.toString();
         }
         else {
             _hour = hour.toString();
         }
-        
     }
 
     function runRules() {
@@ -96,5 +93,50 @@
         getScore: getScore,
         rules: rules
     }
-
 });
+
+(function () {
+    var time = document.querySelector("time"),
+        score = document.getElementById("score"),
+        rules = document.getElementById("rules"),
+        ts = new TimeScore();
+
+    function display() {
+        var result = ts.getScore(new Date());
+        //var result = ts.getScore(new Date(2015, 12, 31, 9, 9));
+
+        time.innerHTML = result.time;
+        score.innerHTML = result.score.points;
+
+        var lis = rules.getElementsByTagName("li");
+
+        for (var i = 0; i < lis.length; i++) {
+            li = lis[i];
+
+            if (result.score.points > 0 && li.innerHTML.lastIndexOf(result.score.rule) > -1) {
+                li.classList.add("active");
+            }
+            else {
+                li.classList.remove("active");
+            }
+        }
+    }
+
+    function showRules() {
+        for (var name in ts.rules) {
+            var rule = ts.rules[name];
+            var li = document.createElement("li");
+            li.innerHTML = rule.points + " points - " + rule.rule;
+            li.id = name;
+            rules.appendChild(li);
+        }
+    };
+
+    showRules();
+    display();
+
+    setInterval(function () {
+        display();
+
+    }, 60 * 1000); // 1 minute
+})();
