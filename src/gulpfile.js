393f68b79e0cf01615e454694bc88b4f799bdbd1
Mads Kristensen <madsk@microsoft.com>
2026-03-08 14:13:53 -0700

    Migrate build to Gulp 4, update manifest generation 🚀
    - Migrated build system to Gulp 4.x with function-based tasks and `gulp.series`/`gulp.watch`
    - Rewrote manifest generation using native Node.js modules, removing `gulp-cache-manifest` dependency
    - Updated `package.json` for Gulp 4.x and added `gulp-cli` as a dev dependency
    - Added `package-lock.json` to lock updated dependencies
    - Regenerated `manifest.appcache` with new build timestamp and output

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,34 +1,81 @@
-﻿/// <binding AfterBuild='default' ProjectOpened='watch' />
+/// <binding AfterBuild='default' ProjectOpened='watch' />
 
 var gulp = require("gulp"),
-    manifest = require("gulp-cache-manifest"),
     concat = require("gulp-concat");
+var fs = require("fs");
+var path = require("path");
 
-gulp.task("default", ["concat", "manifest"]);
-
-
-gulp.task("concat", function () {
-    gulp.src(["./assets/js/badgeService.js", "./assets/js/highscoreService.js", "./assets/js/timescore.js", "./assets/js/dom.js", "./assets/js/appcacheHandler.js"])
+function concatTask() {
+    return gulp.src(["./assets/js/badgeService.js", "./assets/js/highscoreService.js", "./assets/js/timescore.js", "./assets/js/dom.js", "./assets/js/appcacheHandler.js"])
         .pipe(concat("all.js"))
-        .pipe(gulp.dest("./assets/dist"))
-});
+        .pipe(gulp.dest("./assets/dist"));
+}
 
-gulp.task("manifest", function () {
-    gulp.src(["./assets/{dist,css,img}/*.{js,css,svg}", "./index.html", "web.config"])
-    .pipe(manifest({
-        timestamp: true,
-        relativePath: "/assets",
-        preferOnline: true,
-        network: ["*"],
-        filename: "manifest.appcache",
+async function manifestTask() {
+    var cacheEntries = [];
+    var sources = [
+        { folder: "dist", extensions: [".js"] },
+        { folder: "css", extensions: [".css"] },
+        { folder: "img", extensions: [".svg"] }
+    ];
 
-        exclude: ["index.html", "web.config"]
-    }))
-    .pipe(gulp.dest('./'));
-});
+    for (var i = 0; i < sources.length; i++) {
+        var source = sources[i];
+        var directoryPath = path.join(__dirname, "assets", source.folder);
+        var files;
 
-gulp.task("watch", ["concat", "manifest"], function () {
-    gulp.watch("./assets/js/*", ["concat", "manifest"]);
-    gulp.watch("./assets/css/*", ["manifest"]);
-    gulp.watch(["./index.html", "./web.config", "./assets/img/*"], ["manifest"]);
-});
+        try {
+            files = await fs.promises.readdir(directoryPath, { withFileTypes: true });
+        } catch (error) {
+            if (error && error.code === "ENOENT") {
+                continue;
+            }
+
+            throw error;
+        }
+
+        for (var j = 0; j < files.length; j++) {
+            var file = files[j];
+            var extension = path.extname(file.name).toLowerCase();
+
+            if (!file.isFile() || source.extensions.indexOf(extension) === -1) {
+                continue;
+            }
+
+            cacheEntries.push("/assets/" + source.folder + "/" + encodeURI(file.name));
+        }
+    }
+
+    var manifestContent = [
+        "CACHE MANIFEST",
+        "# Time: " + new Date(),
+        "",
+        "",
+        "CACHE:",
+        cacheEntries.join("\n"),
+        "",
+        "",
+        "NETWORK:",
+        "*",
+        "",
+        "",
+        "SETTINGS:",
+        "prefer-online",
+        ""
+    ].join("\n");
+
+    await fs.promises.writeFile(path.join(__dirname, "manifest.appcache"), manifestContent, "utf8");
+}
+
+function watchTask() {
+    gulp.watch("./assets/js/*", gulp.series(concatTask, manifestTask));
+    gulp.watch("./assets/css/*", gulp.series(manifestTask));
+    gulp.watch(["./index.html", "./web.config", "./assets/img/*"], gulp.series(manifestTask));
+}
+
+var build = gulp.series(concatTask, manifestTask);
+
+exports.concat = concatTask;
+exports.manifest = manifestTask;
+exports.default = build;
+exports.watch = gulp.series(build, watchTask);
